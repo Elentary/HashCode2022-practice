a70cf13381411765c89479c5420607c18c51772f
Dmitriy Baranov <amareelez@yandex-team.ru>
2022-02-20 22:21:07 +0300

    Read solutions from files

--- main.cpp
+++ main.cpp
@@ -1,29 +1,47 @@
 #include <iostream>
 #include <fstream>
+#include <cassert>
 
 #include "data.h"
 #include "scorer.h"
 
-
 int main() {
   const std::vector<std::string> files = {
-   "a_an_example.in",
-   "b_basic.in",
-   "c_coarse.in",
-   "d_difficult.in",
-   "e_elaborate.in"
+      "a_an_example.in",
+      "b_basic.in",
+      "c_coarse.in",
+      "d_difficult.in",
+      "e_elaborate.in"
   };
-  
-  for (const auto& file : files) {
-    std::ifstream in(file);
+
+  const std::vector<std::string> output_files = {
+      "a_an_example.in",
+      "b_basic.in",
+      "c_coarse.in",
+      "d_difficult.in",
+      "e_elaborate.in"
+  };
+  assert(files.size() == output_files.size());
+
+  bool read_output_from_files = false;
+
+  for (size_t i = 0; i < files.size(); ++i) {
+    std::ifstream in(files[i]);
     Task task;
     in >> task;
 
     Solution solution;
+    if (read_output_from_files) {
+      std::ifstream in2(output_files[i]);
+      in2 >> solution;
+    }
 
-    std::ofstream out(file.substr(0, file.size() - 3) + ".out");
-    out << solution;
-    std::cout << file << ": " << Scorer::Score(task, solution) << "\n";
+    std::cout << files[i] << ": " << Scorer::Score(task, solution) << "\n";
+
+    if (!read_output_from_files) {
+      std::ofstream out(files[i].substr(0, files[i].size() - 3) + ".out");
+      out << solution;
+    }
   }
   return 0;
 }
